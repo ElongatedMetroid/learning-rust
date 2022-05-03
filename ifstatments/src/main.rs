79edde36f3f9333e1fd5525ffc30aca7d9065f16
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-05-02 20:35:03 -0700

    Add ifstatments example

---
+++ ifstatments/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let n = -1;
+    if n > 0 {
+        println!("Greater than 0");
+    } else if n < 0 {
+        println!("Less than 0");
+    } else {
+        println!("Is 0");
+    }
+}
